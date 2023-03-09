dca5674be509eba774c5e50a33f4169f502c7a75
DavidRodrigues <davidbhz@gmail.com>
2023-03-09 20:14:03 -0300

    Update NotificationClient.java

--- ada-ticketsale-api/src/main/java/com/ada/ticketsaleapi/domain/infra/client/NotificationClient.java
+++ ada-ticketsale-api/src/main/java/com/ada/ticketsaleapi/domain/infra/client/NotificationClient.java
@@ -1,19 +1,8 @@
 package com.ada.ticketsaleapi.domain.infra.client;
 
-//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
-
-//@FeignClient("notifications")
-//@Component 
-//public interface NotificationClient {
-//	
-//    @GetMapping("/notifications/bilhetes/{id}")
-//    void sendNotification(@PathVariable Long long1);
-//
-//}
-
 
 public interface NotificationClient {
 
