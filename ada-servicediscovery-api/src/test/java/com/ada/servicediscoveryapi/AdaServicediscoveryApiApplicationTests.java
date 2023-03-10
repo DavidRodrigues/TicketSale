8293522bed3df95bab6a9b65488e5c70c63683e8
= <=>
2023-03-10 12:01:30 -0300

    criando projeto ada-servicediscovery-api

---
+++ ada-servicediscovery-api/src/test/java/com/ada/servicediscoveryapi/AdaServicediscoveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.servicediscoveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaServicediscoveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
